221533c5d3faae77579a42234d7108f1812636e6
hakida129 <vietlongit@gmail.com>
2021-09-25 01:33:29 +0800

    Design schema database & manage mongodb connection

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,23 +1,20 @@
 import { MongoClient } from 'mongodb'
 import { env } from '*/config/environment'
 
+let dbInstance = null
+
 export const connectDB = async () => {
   const client = new MongoClient(env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-  try {
-    await client.connect()
+  //Connect client to server
+  await client.connect()
 
-    console.log('Connected successfully to server')
-
-    await listDatabases(client)
-
-  } finally {
-    await client.close()
-  }
+  //Assign clientDB to our dbInstance
+  dbInstance = client.db(env.DATABASE_NAME)
 }
 
-const listDatabases = async (client) => {
-  const databasesList = await client.db().admin().listDatabases()
-  console.log('Databases:')
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`))
+//Get database instance
+export const getDB = () => {
+  if (!dbInstance) throw new Error('[Must connect to database first!')
+  return dbInstance
 }
