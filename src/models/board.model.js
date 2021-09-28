686a2c8e5e2cca1518531e7092db0fe02a7c2b7c
hakida129 <vietlongit@gmail.com>
2021-09-28 13:15:56 +0800

    CRUD APIs.

--- src/models/board.model.js
+++ src/models/board.model.js
@@ -4,8 +4,8 @@ import { getDB } from '*/config/mongodb'
 //Define Board collection
 const boardCollectionName = 'boards'
 const boardCollectionSchema = Joi.object({
-  title: Joi.string().required().min(3).max(20),
-  columnOrder: Joi.array().items(Joi.string().default([])),
+  title: Joi.string().required().min(3).max(20).trim(),
+  columnOrder: Joi.array().items(Joi.string()).default([]),
   createdAt: Joi.date().timestamp().default(Date.now()),
   updatedAt: Joi.date().timestamp().default(null),
   _destroy: Joi.boolean().default(false)
